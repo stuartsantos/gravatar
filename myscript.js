2ecbdaa10462cbbb422eca43f17c5cfd1954e4af
stuartsantos <santos.stuart@gmail.com>
2014-10-14 11:22:17 +0100

    402 default
    if no img, show broken img

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 function hash() {
 	var array = $('#email').val().trim().toLowerCase().split("\n");
 	for (var i = 0; i < array.length; i++) {
-		var gravatar = $('<img>').attr({src: 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(array[i])});
+		var gravatar = $('<img>').attr({src: 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(array[i]) + '?d=404'});
 		$("#my_whatever").append(gravatar).append('<br />' + array[i] + '<br />' + i + '<br />' + gravatar.attr("src") + '<br><br />');
 	}
 	return false;
